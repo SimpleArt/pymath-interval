24ae33e98fe5af58a918059f650ac220ae17ebd5
SimpleArt <nguyej17@my.erau.edu>
2022-10-14 23:10:44 -0400

    Updated with new versions

--- src/pymath/interval/_src/fpu_rounding.py
+++ src/pymath/interval/_src/fpu_rounding.py
@@ -76,14 +76,14 @@ def split_precision(x: float) -> Tuple[float, float, float]:
     x_large = x_mantissa - x_small
     return (x_exponent, x_small, x_large)
 
-def float_split(x: SupportsRichFloat) -> tuple[float, float]:
+def float_split(x: SupportsRichFloat) -> Tuple[float, float]:
     y = float(x)
     if x < y:
-        L = math.nextafter(y, -math.inf)
+        L = nextafter(y, -math.inf)
         U = y
     elif x > y:
         L = y
-        U = math.nextafter(y, math.inf)
+        U = nextafter(y, math.inf)
     else:
         L = U = y
     return (L, U)
@@ -91,18 +91,18 @@ def float_split(x: SupportsRichFloat) -> tuple[float, float]:
 def float_down(x: SupportsRichFloat) -> float:
     y = float(x)
     if x < y:
-        return math.nextafter(y, -math.inf)
+        return nextafter(y, -math.inf)
     else:
         return y
 
 def float_up(x: SupportsRichFloat) -> float:
     y = float(x)
     if x > y:
-        return math.nextafter(y, math.inf)
+        return nextafter(y, math.inf)
     else:
         return y
 
-def multi_add(*args: float) -> list[float]:
+def multi_add(*args: float) -> List[float]:
     partials = []
     remaining = sorted(args)
     for _ in range(len(args)):
@@ -112,7 +112,7 @@ def multi_add(*args: float) -> list[float]:
             partials_add(partials, remaining.pop(0))
     return partials
 
-def partials_add(partials: list[float], x: float) -> list[float]:
+def partials_add(partials: List[float], x: float) -> List[float]:
     i = 0
     for y in partials:
         if abs(x) < abs(y):
@@ -131,7 +131,7 @@ def partials_add(partials: list[float], x: float) -> list[float]:
         del partials[-1]
     return partials
 
-def partials_times(partials: list[float], x: float) -> list[float]:
+def partials_times(partials: List[float], x: float) -> List[float]:
     temp = []
     for y in partials:
         for z in mul_precise(x, y):
@@ -139,7 +139,7 @@ def partials_times(partials: list[float], x: float) -> list[float]:
     partials[:] = temp
     return partials
 
-def add_precise(x: float, y: float) -> list[float]:
+def add_precise(x: float, y: float) -> List[float]:
     if abs(x) < abs(y):
         x, y = y, x
     total = x + y
@@ -152,7 +152,7 @@ def add_down(x: float, y: float) -> float:
     else:
         partials = add_precise(x, y)
     if len(partials) > 1 and partials[-2] < 0.0:
-        return math.nextafter(partials[-1], -math.inf)
+        return nextafter(partials[-1], -math.inf)
     else:
         return partials[-1]
 
@@ -162,7 +162,7 @@ def add_up(x: float, y: float) -> float:
     else:
         partials = add_precise(x, y)
     if len(partials) > 1 and partials[-2] > 0.0:
-        return math.nextafter(partials[-1], math.inf)
+        return nextafter(partials[-1], math.inf)
     else:
         return partials[-1]
 
@@ -187,7 +187,7 @@ def mul_down(x: float, y: float) -> float:
     if len(partials) == 1 or partials[-2] >= 0.0:
         return partials[-1]
     else:
-        return math.nextafter(partials[-1], -math.inf)
+        return nextafter(z, -math.inf)
 
 def mul_up(x: float, y: float) -> float:
     if x * y == -math.inf and not math.isinf(x) and not math.isinf(y):
@@ -196,7 +196,7 @@ def mul_up(x: float, y: float) -> float:
     if len(partials) == 1 or partials[-2] <= 0.0:
         return partials[-1]
     else:
-        return math.nextafter(partials[-1], math.inf)
+        return nextafter(z, math.inf)
 
 def div_down(x: float, y: float) -> float:
     if y == 0.0:
@@ -208,29 +208,29 @@ def div_down(x: float, y: float) -> float:
             return 0.0
     quotient = x / y
     if quotient == math.inf and not math.isinf(x):
-        return math.nextafter(math.inf, 0.0)
+        return nextafter(math.inf, 0.0)
     elif math.isinf(quotient):
         return quotient
     elif quotient != 0.0:
         partials = mul_precise(quotient, y)
         if y > 0:
             if partials[-1] > x:
-                return math.nextafter(quotient, -math.inf)
             elif partials[-1] < x or len(partials) == 1 or partials[-2] < 0.0:
+                return nextafter(quotient, -math.inf)
                 return quotient
             else:
-                return math.nextafter(quotient, -math.inf)
+                return nextafter(quotient, -math.inf)
         else:
             if partials[-1] < x:
-                return math.nextafter(quotient, -math.inf)
             elif partials[-1] > x or len(partials) == 1 or partials[-2] > 0.0:
+                return nextafter(quotient, -math.inf)
                 return quotient
             else:
-                return math.nextafter(quotient, -math.inf)
+                return nextafter(quotient, -math.inf)
     elif x == 0.0 or math.isinf(y) or not (x < 0.0 < y or y < 0.0 < x):
         return quotient
     else:
-        return math.nextafter(0.0, -math.inf)
+        return nextafter(0.0, -math.inf)
 
 def div_up(x: float, y: float) -> float:
     if y == 0.0:
@@ -242,35 +242,35 @@ def div_up(x: float, y: float) -> float:
             return math.inf
     quotient = x / y
     if quotient == -math.inf and not math.isinf(x):
-        return math.nextafter(-math.inf, 0.0)
+        return nextafter(-math.inf, 0.0)
     elif math.isinf(quotient):
         return quotient
     elif quotient != 0.0:
         partials = mul_precise(quotient, y)
         if y > 0:
             if partials[-1] < x:
-                return math.nextafter(quotient, math.inf)
             elif partials[-1] > x or len(partials) == 1 or partials[-2] > 0.0:
+                return nextafter(quotient, math.inf)
                 return quotient
             else:
-                return math.nextafter(quotient, math.inf)
+                return nextafter(quotient, math.inf)
         else:
             if partials[-1] > x:
-                return math.nextafter(quotient, math.inf)
+                return nextafter(quotient, math.inf)
             elif partials[-1] < x or len(partials) == 1 or partials[-2] < 0.0:
                 return quotient
             else:
-                return math.nextafter(quotient, math.inf)
+                return nextafter(quotient, math.inf)
     elif x == 0.0 or math.isinf(y) or x < 0.0 < y or y < 0.0 < x:
         return quotient
     else:
-        return math.nextafter(0.0, math.inf)
+        return nextafter(0.0, math.inf)
 
 def pow_down(x: float, y: float) -> float:
     try:
         result = math.pow(x, y)
     except OverflowError:
-        return math.nextafter(x ** (y % 2) * math.inf, -math.inf)
+        return nextafter(x ** (y % 2) * math.inf, -math.inf)
     except ValueError:
         if y == round(y) and round(y) % 2 == 1:
             return -math.inf
@@ -285,7 +285,7 @@ def pow_down(x: float, y: float) -> float:
     ):
         return result
     elif Decimal(x) ** Decimal(y) < result:
-        return math.nextafter(result, -math.inf)
+        return nextafter(result, -math.inf)
     else:
         return result
 
@@ -293,7 +293,7 @@ def pow_up(x: float, y: float) -> float:
     try:
         result = math.pow(x, y)
     except OverflowError:
-        return math.nextafter(x ** (y % 2) * math.inf, math.inf)
+        return nextafter(x ** (y % 2) * math.inf, math.inf)
     except ValueError:
         return math.inf
     if (
@@ -305,6 +305,6 @@ def pow_up(x: float, y: float) -> float:
     ):
         return result
     elif Decimal(x) ** Decimal(y) > result:
-        return math.nextafter(result, math.inf)
+        return nextafter(result, math.inf)
     else:
         return result
